61afb8568a02fa75c04ae2c286bb4b454c67ab99
GitPythZ <yardbirds1957@gmail.com>
2024-08-10 00:10:33 +0300

    ЙО ХОХОХОХОХО и Бутылка РОМА!

---
+++ Beersong.py
@@ -0,0 +1,20 @@
+beernumber = 99
+word = "бутылок (бутылки) "
+while beernumber > 0:
+    if beernumber == 1:
+        word = "бутылка "
+    print(beernumber, " " + word + "пива на стене")
+    print(beernumber, " " + word + "пива")
+    print("Возьми одну бутылочку")
+    print("Пусти ее по кругу")
+    beernumber = beernumber - 1
+else:
+    print("Бутылок больше нет((")
+
+# a = (12)//10
+# print(a)
+
+
+
+
+
